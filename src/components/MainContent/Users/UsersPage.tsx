d1e81b6355755fcf1a07e0893af683c67ea4fcd2
danila-beep <danil123w29@gmail.com>
2023-07-16 16:08:49 +0300

    Step 5 Api first requests | UserReducer refactor

--- src/components/MainContent/Users/UsersPage.tsx
+++ src/components/MainContent/Users/UsersPage.tsx
@@ -1,36 +1,78 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from '../../../store/store'
-import { UserPageType, UserType, followUserAC, unfollowUserAC } from '../../../store/reducers/usersReducer'
-import { Button } from '../../../shared/Button'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../store/store";
+import {
+  UserPageType,
+  followUserAC,
+  setUsersAC,
+  unfollowUserAC,
+} from "../../../store/reducers/usersReducer";
+import { Button } from "../../../shared/Button";
+import axios from "axios";
 
 const UsersPage = () => {
-  const usersPageData = useSelector<RootState, UserPageType>(state => state.usersPage)
-  const dispatch = useDispatch()
+  const usersPageData = useSelector<RootState, UserPageType>(
+    (state) => state.usersPage
+  );
+  const dispatch = useDispatch();
 
+  const baseURL = "https://social-network.samuraijs.com/api/1.0";
+
+  useEffect(() => {
+    const getUsersFromApi = async () => {
+      try {
+        axios.get(`${baseURL}/users`).then((response) => {
+          const data = response.data;
+          console.log(data);
+          dispatch(setUsersAC(data));
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUsersFromApi();
+  }, []);
 
   return (
-    <div>{usersPageData.users.map(user => {
-      console.log(usersPageData);
-      console.log(user.isFollowed);
-      return (
-        <div key={user.id} style={{display: "inline-block", padding: "10px", margin: "10px", border: "1px solid black"}}>
-          <div>
-            <img src={user.userPhoto} alt="" />
-          </div>
-          <span>{user.firstName}</span>
-          <span>{user.lastName}</span>
-          <div>
-          <Button onClick={() => dispatch(followUserAC(user.id))}>Follow</Button>
-          <Button onClick={() => dispatch(unfollowUserAC(user.id))}>Unfollow</Button>
-          </div>
-          <div>
-            {user.isFollowed.toString()}
-          </div>
-        </div>
-      )
-    })}</div>
-  )
-}
+    <div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        {usersPageData.users.items.map((user) => {
+          return (
+            <div
+              key={user.id}
+              style={{
+                display: "inline-block",
+                padding: "10px",
+                margin: "10px",
+                border: "1px solid black",
+              }}
+            >
+              <div>
+                <img
+                  src={
+                    user.photos.small
+                      ? user.photos.small
+                      : "https://www.istockphoto.com/photos/user-profile"
+                  }
+                  alt=""
+                />
+              </div>
+              <span>{user.name}</span>
+              <div>
+                <Button onClick={() => dispatch(followUserAC(user.id))}>
+                  Follow
+                </Button>
+                <Button onClick={() => dispatch(unfollowUserAC(user.id))}>
+                  Unfollow
+                </Button>
+              </div>
+              <div>{user.followed.toString()}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default UsersPage
+export default UsersPage;
